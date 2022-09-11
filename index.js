3baaaa9176dd6387ba198c69d1fb257f4cdc9550
sanditomaz <sandi.contato@gmail.com>
2022-09-11 00:26:25 -0300

    Entry Setups Finished

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import joi from "joi";
 import dayjs from "dayjs";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -29,6 +31,11 @@ const loginSchema = joi.object({
   password: joi.string().min(1).required(),
 });
 
+const transactionSchema = joi.object({
+  description: joi.string().trim().min(1).required(),
+  value: joi.string().trim().min(1).required(),
+});
+
 app.post("/register", async (req, res) => {
   const validation = signUpSchema.validate(req.body, {
     abortEarly: false,
@@ -45,6 +52,8 @@ app.post("/register", async (req, res) => {
     return;
   }
 
+  const hashPassword = bcrypt.hashSync(req.body.password, 10);
+
   try {
     const userEmail = await db.collection("userData").find().toArray();
 
@@ -55,7 +64,11 @@ app.post("/register", async (req, res) => {
       return;
     }
 
-    await db.collection("userData").insertOne(req.body);
+    await db.collection("userData").insertOne({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassword,
+    });
 
     res.sendStatus(201);
   } catch (error) {
@@ -79,18 +92,160 @@ app.post("/login", async (req, res) => {
 
     const validUser = userEmail.find(
       (value) =>
-        value.email === req.body.email && value.password === req.body.password
+        value.email === req.body.email &&
+        bcrypt.compareSync(req.body.password, value.password)
     );
 
     if (!validUser) {
-      res.sendStatus(409);
+      res.sendStatus(401);
       return;
     }
+
+    const token = {
+      name: validUser.name,
+      token: uuidv4(),
+      userId: validUser._id,
+    };
+
+    db.collection("sessions").insertOne(token);
+
+    res.send(token);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.post("/addmoney", async (req, res) => {
+  const validation = transactionSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(errors);
+    return;
+  }
+
+  const token = req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.send(401);
+  }
+
+  try {
+    const session = await db.collection("sessions").findOne({
+      token,
+    });
+
+    if (!session) {
+      return res.send(401);
+    }
+
+    const user = await db.collection("userData").findOne({
+      _id: session.userId,
+    });
+
+    if (user) {
+      delete user.password;
+    }
+
+    const insertMoney = {
+      value: req.body.value,
+      description: req.body.description,
+      status: "green",
+      date: dayjs().format("MM/DD/YYYY"),
+      userId: user._id,
+    };
+
+    db.collection("transactions").insertOne(insertMoney);
 
     res.sendStatus(201);
   } catch (error) {
     res.sendStatus(500);
   }
+});
+
+app.post("/withdraw", async (req, res) => {
+  const validation = transactionSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    res.status(422).send(errors);
+    return;
+  }
+
+  const token = req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.send(401);
+  }
+
+  try {
+    const session = await db.collection("sessions").findOne({
+      token,
+    });
+
+    if (!session) {
+      return res.send(401);
+    }
+
+    const user = await db.collection("userData").findOne({
+      _id: session.userId,
+    });
+
+    if (user) {
+      delete user.password;
+    }
+
+    const withdrawal = {
+      value: req.body.value,
+      description: req.body.description,
+      status: "red",
+      date: dayjs().format("MM/DD/YYYY"),
+      userId: user._id,
+    };
+
+    db.collection("transactions").insertOne(withdrawal);
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+
+app.get("/home", async (req, res) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.send(401);
+  }
+
+  try {
+    const session = await db.collection("sessions").findOne({
+      token,
+    });
+
+    if (!session) {
+      return res.send(401);
+    }
+
+    const user = await db.collection("userData").findOne({
+      _id: session.userId,
+    });
+
+    if (user) {
+      delete user.password;
+      return res.send(200);
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+  res.sendStatus(200);
 });
 
 app.listen(5000, () => console.log("Listening on port 5000"));
